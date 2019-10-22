a18e7afc24b219e2c9f2a3f3ec270900d57b67d8
Dominik Zig <dominikzig@hotmail.co.nz>
2019-10-22 23:44:27 +1300

    Added frontend build tools: node, npm, webpack.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/app.js', //app.js is essentially the main() equivalent in java
+    devtool: 'sourcemaps',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js' //bundle.js is essentially a Spring Boot uber JAR, basically all js code and dependencies are stored in it
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    }
+};
