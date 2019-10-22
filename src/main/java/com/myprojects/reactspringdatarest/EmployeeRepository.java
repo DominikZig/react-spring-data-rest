699bfd9f9c612ea3b8140f036f32eaeea7e68ae7
Dominik Zig <dominikzig@hotmail.co.nz>
2019-10-22 17:54:36 +1300

    Created REST api.

--- src/main/java/com/myprojects/reactspringdatarest/EmployeeRepository.java
+++ src/main/java/com/myprojects/reactspringdatarest/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.myprojects.reactspringdatarest;
 
 import org.springframework.data.repository.CrudRepository;
 
+//Extending CrudRepository which has all CRUD methods already
 public interface EmployeeRepository extends CrudRepository<Employee, Long>
 {
 }
--- src/main/java/com/myprojects/reactspringdatarest/ReactSpringDataRestApplication.java
+++ src/main/java/com/myprojects/reactspringdatarest/ReactSpringDataRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactSpringDataRestApplication {
-
-	public static void main(String[] args) {
+public class ReactSpringDataRestApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ReactSpringDataRestApplication.class, args);
 	}
-
 }
