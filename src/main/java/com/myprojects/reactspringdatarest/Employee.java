961da1d77bbf6a23238cf99eac47a816efd6e5ed
Dominik Zig <dominikzig@hotmail.co.nz>
2019-10-28 15:29:14 +1300

    Added conditional PUT functionality on frontend.

--- src/main/java/com/myprojects/reactspringdatarest/Employee.java
+++ src/main/java/com/myprojects/reactspringdatarest/Employee.java
@@ -1,8 +1,11 @@
 package com.myprojects.reactspringdatarest;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 import java.util.Objects;
 
 //@Entity is a JPA annotation that denotes the whole class for storage in a relational table.
@@ -13,6 +16,8 @@ public class Employee
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore Long version;
 
     private Employee()
     {
@@ -44,13 +49,14 @@ public class Employee
         return Objects.equals(id, employee.id) &&
                 Objects.equals(firstName, employee.firstName) &&
                 Objects.equals(lastName, employee.lastName) &&
-                Objects.equals(description, employee.description);
+                Objects.equals(description, employee.description) &&
+                Objects.equals(version, employee.version);
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash(id, firstName, lastName, description);
+        return Objects.hash(id, firstName, lastName, description, version);
     }
 
     public Long getId()
@@ -93,6 +99,16 @@ public class Employee
         this.description = description;
     }
 
+    public Long getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion(Long version)
+    {
+        this.version = version;
+    }
+
     @Override
     public String toString()
     {
@@ -101,6 +117,7 @@ public class Employee
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", description='" + description + '\'' +
+                ", version=" + version +
                 '}';
     }
 }
